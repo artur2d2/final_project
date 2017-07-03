b4aac33ca70de44c00b56820ba8fdb38761ae844
Arturo Gabriel Hernandez Garcia <artur2d2@msn.com>
2017-07-03 23:52:49 +0000

    Adding Pagedraw templates and other backend files

---
+++ application.py
@@ -0,0 +1,43 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import gettempdir
+from datetime import datetime
+
+from helpers import *
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# custom filter
+app.jinja_env.filters["usd"] = usd
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///project.db")
+
+## Routes go here
+# A route should be of the form
+#
+# @app.route("/foo")
+# def foo():
+#     username = 'Garfunkel'
+#     return render_template("index.html", name=username)
+#
+# where "project/templates/index.html" is a file created automatically by Pagedraw.
+# Within the respective Pagedraw document, you can do {{ name }} which will print 'Garfunkel'
